65cb34dd8d4482b935ddee98b3bb19b5e13b95d0
AjaybalaC <ajay.c.ad.2021@snsce.ac.in>
2026-01-04 12:08:11 +0530

    Update the allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: true,
+    allowedHosts: [
+      "pastebin-frontend-gn7h.onrender.com"
+    ]
+  }
+});
